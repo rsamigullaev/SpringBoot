5e023c571f11a9a37ec51c068f800b7aa6dc2cbd
rsamigullaev <samigullaev@inbox.ru>
2023-05-06 17:47:31 +0300

    add commit

---
+++ src/main/java/ru/netology/springboot/config/JavaConfig.java
@@ -0,0 +1,24 @@
+package ru.netology.springboot.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.springboot.profile.DevProfile;
+import ru.netology.springboot.profile.ProductionProfile;
+import ru.netology.springboot.profile.SystemProfile;
+
+@Configuration
+public class JavaConfig {
+
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "true")
+    @Bean
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "false")
+    @Bean
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
